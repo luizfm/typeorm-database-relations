3d6d76195114fa0ace35fe86994c675149f89a88
Luiz <luiz.ado@hotmail.com>
2020-12-08 23:16:30 -0300

    Added the migrations and created the table columns on database. Started to change some information on Customers.

---
+++ src/shared/infra/typeorm/migrations/1607477337967-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddProductIdToOrdersProducts1607477337967
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsProduct',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('order_products', 'OrdersProductsProduct');
+
+    await queryRunner.dropColumn('order_products', 'product_id');
+  }
+}
